fa7cce7cbb1adb909b3d6cab114f1bce41bb7fb7
isharipov <prohodka67@gmail.com>
2016-04-05 23:19:13 +0300

    fix

--- src/main/java/com/isharipov/service/CurrentUserDetailService.java
+++ src/main/java/com/isharipov/service/CurrentUserDetailService.java
@@ -2,6 +2,7 @@ package com.isharipov.service;
 
 import com.isharipov.domain.User;
 import com.isharipov.utils.CurrentUser;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by Илья on 05.04.2016.
  */
+@Slf4j
 @Service
 public class CurrentUserDetailService implements UserDetailsService {
 
@@ -20,6 +22,7 @@ public class CurrentUserDetailService implements UserDetailsService {
     public CurrentUser loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userService.getUserByEmail(email).orElseThrow(
                 () -> new UsernameNotFoundException(String.format("User with email=%s was not found", email)));
+        log.info("User with email: {}, found", email);
         return new CurrentUser(user);
     }
 }
