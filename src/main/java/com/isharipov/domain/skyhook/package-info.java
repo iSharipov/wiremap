ae6a3acbc21faec7a66aa0e663ba8b34a5c08b20
isharipov <prohodka67@gmail.com>
2016-04-17 22:10:19 +0300

    added package info to delete ns2 prefix

---
+++ src/main/java/com/isharipov/domain/skyhook/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Илья on 17.04.2016.
+ */
+@XmlSchema(namespace = "http://skyhookwireless.com/wps/2005",
+        xmlns = {
+                @XmlNs(namespaceURI = "http://skyhookwireless.com/wps/2005", prefix = "")
+        },
+        elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+
+package com.isharipov.domain.skyhook;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlSchema;
